ef93fe27dc75c603c05be6bd9331b708ef0b60dc
Jensen Tonne <jensen.tonne@gmail.com>
2014-07-16 11:56:00 +0800

    Added jquery.domManip.

---
+++ src/jquery.domManip.js
@@ -0,0 +1,23 @@
+/**
+ * jquery.fn.domManip
+ * Shorthands for common DOM operations.
+ *
+ * Part of the jQuery Utils family:
+ * https://github.com/jstonne/jquery.utils
+ *
+ * Copyright (c) 2014 Jensen Tonne
+ * http://jstonne.me
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+
+$.fn.tagName = function(){
+    return this[0].tagName;
+};
+
+$.create = function(tagName) {
+    return $(document.createElement(tagName));
+};
